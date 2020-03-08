9a44b9b747b84b7ea4eeaee09f348ceca3b166e2
Józek <przyjemniaczek@gmail.com>
2020-03-09 00:20:04 +0100

    Implemented userReducer and userActions to header and app components.
    Now the user session persistance is fully functional like pre-redux, but handled
    by redux's data flow and not by app's state object, eliminating the prop drilling that was present earlier (passing state to header component).
    
    Check console for proof provided by redux logger middleware.

--- src/App.js
+++ src/App.js
@@ -7,26 +7,22 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header-component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { connect } from 'react-redux';
+import {setCurrentUser} from './redux/user/user.actions'
 
 
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
@@ -35,7 +31,7 @@ class App extends React.Component {
         })
       }
 
-      this.setState({currentUser : userAuth});
+      setCurrentUser(userAuth);
     })
   }
 
@@ -57,4 +53,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null,mapDispatchToProps)(App);
