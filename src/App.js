386a780dbf569b0fcf2d42a37472713a55000544
Józek <przyjemniaczek@gmail.com>
2020-03-09 17:39:23 +0100

    Added redirect to homapage from sign in page if one is already logged in
    Also added user.types.js to have user action type names used by redux be constants, instead
    of hardcoded strings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import './App.css';
-
+//page components imports
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header-component'
+//auth imports
 import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+//Redux imports
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions'
 
@@ -46,15 +48,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to='/' />):(<SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+//router imports
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+//redux imports
 import { Provider } from 'react-redux'
 import store from './redux/store'
-
+//standard imports
 import './index.css';
 import App from './App';
 
