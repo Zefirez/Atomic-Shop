8771eeef212f7fb6dd2a504c64e36cfc8f9d0e8b
Józek <przyjemniaczek@gmail.com>
2020-03-09 23:37:30 +0100

    Added addItem functionality to CollectionItem Component.
    Now you can actually add items to shoppinng cart by pressing "Add to cart" button.
    This is handled by redux again, with extra code for the purpose added in already
    existing cart files in redux folder. Also modifications were made to CollectionPreview and Item components to allow passing of the selected item object information
    to the function in CollectionItem component that will fire off the addItem action.

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,11 +1,12 @@
 import CartActionTypes from './cart.types'
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
 
             return {
@@ -13,8 +14,14 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 hidden: !state.hidden
             }
 
-            default:
-                return state;
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: [...state.cartItems, action.payload]
+            }
+
+        default:
+            return state;
     }
 }
 
