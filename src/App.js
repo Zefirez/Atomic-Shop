d5044e0a0a71f5fe7519e0188b15750b96e29a2e
Józek <przyjemniaczek@gmail.com>
2020-03-14 18:44:57 +0100

    Added checkout page
    App.js and CartDropdown received routing to this new page (dropdown in it's button)
    CartDropdown also receive default message for display when cart is empty.
    
    Menu item component is of no importance - i just optimized it's css class display code a bit, not related
    to checkout page whatsoever.
    
    Checkout page itself now has bare bones structure, with item listing "table" (not a real html table mind you)
    having it's headers and item names display for testing purposes.
    
    On the data flow end selectors work just fine and new one has been added to cart.selectors.js for total price of all
    items in cart.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header-component'
+import CheckoutPage from './pages/checkout/checkout.component'
 //auth imports
 import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
@@ -51,7 +52,8 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
-        </Switch>
+          <Route exact path='/checkout' component={CheckoutPage} />
+          </Switch>
       </div>
     );
   }
