26d8ff8f10a82ce99f8f2b6ac81f64afee132e47
Józek <przyjemniaczek@gmail.com>
2020-03-16 11:06:55 +0100

    Added localStorage persistance for cart items with redux-persist module
    npm add redux-persist.
    
    Quite a bit of configuration required, but in the end the cart items get saved to
    localStorage and repopulated on page refresh or opening the shop page after closing it.
    
    No one is gonna buy at shop that loses it's cart items cause user refreshed the page or
    accidently closed it and now would be forced to find and add them to cart all over again, right?
    
    Configuration is done in store.js and root-reducer, while index.js applies the now configured
    persistance with <PersistGate> tags wrapping the <App />

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,23 @@
 import {combineReducers} from 'redux';
+import { persistReducer } from 'redux-persist';
+
+//imports localStorage. SessionStorage is in another package
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    //this means that it'll be using localStorage to save data
+    storage,
+    //this is the list of reducers you want saved in your storage
+    whiteList: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer);
