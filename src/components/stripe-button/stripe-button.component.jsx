5a867d010ae2e67f867936dee928cd1e1f25f770
Józek <przyjemniaczek@gmail.com>
2020-03-18 09:28:24 +0100

    Added Stripe payment platform integration
    Which..pretty much completes the functionality of the shop!
    Now it's just polishes and optimizations from this point on. Gj bro!
    
    Oh also small touches in index.html - changed title for one and added matching favicon.
    On that subject - firefox would not display it correctly until you append "?" to it's
    extension - favicon.ico?. Now it's all peachy:3

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout'
+
+import './stripe-button.styles.scss'
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_eADZ9ec2176LqCWCU6rAtCjr00hthZc4nB';
+
+    const onToken = token => {
+        console.log(token)
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+        label='Pay Now'
+        name='Atomic Shop'
+        billingAddress
+        shippingAddress
+        image='https://svgshare.com/i/CUz.svg'
+        description={`Your total is $${price}`}
+        amount={priceForStripe}
+        panelLabel = 'Pay Now'
+        token = {onToken}
+        stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector
 
 import './checkout.styles.scss'
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 
 const CheckoutPage = ({ cartItems, cartTotal }) => (
     <div className="checkout-page">
@@ -35,6 +36,10 @@ const CheckoutPage = ({ cartItems, cartTotal }) => (
         <div className='total'>
             <span>TOTAL: ${cartTotal}</span>
         </div>
+        <div className='test-warning'>*Please use the following credit card for payments*<br />
+        Number: 4242 4242 4242 4242 - Exp: future date - CVV: 123
+        </div>
+        <StripeCheckoutButton price={cartTotal} />
     </div>
 )
 
