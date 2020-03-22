8b31e6250d6087a306d5cb7b0d49356299281779
Józek <przyjemniaczek@gmail.com>
2020-03-22 15:34:09 +0100

    Used container pattern to move out spinner displaying logic outta shop component.
    Now the new container components wrap around CollectionOverview and CollectionPage
    components handling wrapping them in  spinner component and providing data it needs
    to decide when to display and when just pass child component.
    
    This moves component related logic outta shop page that should not be focusing on it
    from the first place.
    
    Also used new helper library from redux - compose. It queques curring functions as
    argumetns without going trough nested parenthesis hell.

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,20 @@
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
+
+import { compose } from "redux";
+import WithSpinner from "../with-spinner/with-spinner.component";
+import CollectionsOverview from "../collections-overview/collections-overview.component";
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: selectIsCollectionFetching
+});
+
+// const CollectionsOverviewContainer = 
+// connect(mapStateToProps)(WithSpinner(CollectionsOverview));
+
+//does same thing as above commented code.
+const CollectionsOverviewContainer = 
+compose(connect(mapStateToProps),WithSpinner)(CollectionsOverview);
+
+export default CollectionsOverviewContainer;
