3e42be1a01159cef08e3c38f5bf4ba5fd0643b5d
Józek <przyjemniaczek@gmail.com>
2020-03-25 19:34:14 +0100

    Created cart.sagas.js to handle clearing cart when user logs out.
    Additionally converted shop.sagas.js to include them in root.saga.js
    similarly as other sagas, rather then just exporting their one function out.

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,42 +1,46 @@
-import CartActionTypes from './cart.types'
-import { addItemToCart, removeItemFromCart } from './cart.utils'
-import CardActionTypes from './cart.types'
-
+import CartActionTypes from "./cart.types";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
-    hidden: true,
-    cartItems: []
-}
+  hidden: true,
+  cartItems: []
+};
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case CartActionTypes.TOGGLE_CART_HIDDEN:
+  switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden
+      };
 
-            return {
-                ...state,
-                hidden: !state.hidden
-            }
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
 
-        case CartActionTypes.ADD_ITEM:
-            return {
-                ...state,
-                cartItems: addItemToCart(state.cartItems, action.payload)
-            }
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(item => item.id !== action.payload.id)
+      };
 
-        case CardActionTypes.CLEAR_ITEM_FROM_CART:
-            return {
-                ...state,
-                cartItems: state.cartItems.filter(item => item.id !== action.payload.id)
-            }
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
 
-        case CardActionTypes.REMOVE_ITEM:
-            return {
-                ...state,
-                cartItems: removeItemFromCart(state.cartItems,action.payload)
-            }
-        default:
-            return state;
-    }
-}
+    case CartActionTypes.CLEAR_CART:
+      return {
+        ...state,
+        cartItems: []
+      };
+      
+    default:
+      return state;
+  }
+};
 
-export default cartReducer
+export default cartReducer;
