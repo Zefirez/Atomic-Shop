85486e2e185fc82af5e925f0dd4bac200106bd74
Józek <przyjemniaczek@gmail.com>
2020-03-30 09:59:12 +0200

    reverted collection component to original state

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,38 +1,27 @@
-import React, { useEffect } from "react";
-import "./collection.styles.scss";
-import { connect } from "react-redux";
-import { selectCollection } from "../../redux/shop/shop.selectors";
+import React from 'react';
+import './collection.styles.scss'
+import { connect } from 'react-redux'
+import {selectCollection} from '../../redux/shop/shop.selectors'
 
-import {firestore} from '../../firebase/firebase.utils'
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import CollectionItem from "../../components/collection-item/collection-item.component";
+const CollectionPage = ({collection}) => {
+    const {title,items} = collection;
 
-const CollectionPage = ({ collection }) => {
-    useEffect(()=>{
-        console.log('I am subscribing')
-        const unsubscribeFromCollections = firestore.collection('collections').onSnapshot(snapshot => console.log(snapshot));
-        return () => {
-            console.log('I am unsubscribing');
-            unsubscribeFromCollections();
+    return (<div className='collection-page'>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+        {
+            items.map(item => 
+                <CollectionItem item={item} key={item.id}/>
+            )
         }
-    },[])
-
-  const { title, items } = collection;
-
-  return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
-        {items.map(item => (
-          <CollectionItem item={item} key={item.id} />
-        ))}
-      </div>
+        </div>
     </div>
-  );
-};
+)}
 
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
 
 export default connect(mapStateToProps)(CollectionPage);
